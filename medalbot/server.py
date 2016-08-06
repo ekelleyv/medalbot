4d89a22db7f4223003297eaf288385ab6724719f
Ed Kelley <ekelleyv@gmail.com>
2016-08-06 15:51:00 -0400

    Added docs/homepage

--- medalbot/server.py
+++ medalbot/server.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Welcome to medalbot"
+def docs():
+    return app.send_static_file('endpoints.html')
 
 
 @app.route("/api/v1/medals")
@@ -15,4 +15,5 @@ def medals():
     return jsonify(get_counts())
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
