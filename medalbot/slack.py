826fa1e5a4eebed9f1eb33844ec35c3591383203
Vaibhav Kaul <vaibhav@seatgeek.com>
2016-08-18 10:34:54 -0400

    india will always show up

--- medalbot/slack.py
+++ medalbot/slack.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 
-from medalbot.get_counts import get_counts
+from medalbot.get_counts import get_counts, get_counts_by_country_id
 
 
 def get_slack_counts():
@@ -17,6 +17,16 @@ def get_slack_counts():
             count["bronze_count"],
             count["total_count"]
         ])
+
+    india_count = get_counts_by_country_id("india")
+    response_table.append([
+        india_count["place"],
+        india_count["country_name"],
+        india_count["gold_count"],
+        india_count["silver_count"],
+        india_count["bronze_count"],
+        india_count["total_count"]
+    ])
 
     # Use block so things align
     return "```{0}```".format(tabulate(response_table, headers))
